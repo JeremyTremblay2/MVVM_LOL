357ee5d413cbbf6a0d9e8f4ea99df45ca6f14a2f
Jérémy TREMBLAY <jeremy.tremblay@etu.uca.fr>
2023-06-17 14:24:16 +0200

    [#2] Add a first version of the vm toolkit

---
+++ Sources/VMToolkit/BaseVM.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace VMToolkit;
+
+public  class BaseVM : INotifyPropertyChanged //<T> where T : IEquatable<T>, INotifyPropertyChanged
+{
+    //protected T Model { get; private set; }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    /*public BaseVM(T model)
+    {
+        Model = model;
+    }*/
+
+    public void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+}
